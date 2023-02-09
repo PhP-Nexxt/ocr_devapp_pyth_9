41ea2d4db2fab14902d0318edbfe899ef2e9e338
philippe_pras <ph.pras@gmail.com>
2023-02-09 16:38:34 +0100

    Update

--- litreview_dj/ocr_project_9/settings.py
+++ litreview_dj/ocr_project_9/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "litreview_app"
+    "litreview_app", #AddApp project9
 ]
 
 MIDDLEWARE = [
