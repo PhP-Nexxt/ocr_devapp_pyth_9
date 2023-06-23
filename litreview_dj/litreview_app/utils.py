d0c44e76d324e288ae9044497ff9281411eb8bf2
philippe_pras <ph.pras@gmail.com>
2023-06-23 18:03:43 +0200

    Update filtres et tri flux

--- litreview_dj/litreview_app/utils.py
+++ litreview_dj/litreview_app/utils.py
@@ -1,13 +1,21 @@
 # Fichier de fonctions annexes
 
-from litreview_app.models import Review, Ticket
+from django.db.models import Q #Objet "Queryset" permet d'écrire des conditions
+from litreview_app.models import Review, Ticket, UserFollows
 
 
 
 def get_users_viewable_reviews(user):
-    reviews = Review.objects.all() # récupérer toutes les critiques
+    myfollows = UserFollows.objects.filter(followed_user=user) #Filtre Les gens a qui je suis abonné
+    myfollows_user = [myfollow.user for myfollow in myfollows] #Iteration sur les gens que je suis (liste)
+    reviews = Review.objects.filter(Q(user__in=myfollows_user) | Q(user=user)) # récupérer toutes les critiques triée 1Les personne que je suis (Q1) et 2les review que j'ai crées(Q2) |=ou
     return reviews
 
 def get_users_viewable_tickets(user):
-    tickets = Ticket.objects.all() # recuperer tous les tickets
+    myfollows = UserFollows.objects.filter(followed_user=user) #Filtres Les gens a qui je suis abonné
+    myfollows_user = [myfollow.user for myfollow in myfollows] #Iteration sur les gens que je suis
+    tickets = Ticket.objects.filter(Q(user__in=myfollows_user) | Q(user=user)) # # récupérer toutes les tickets 1Les personne que je suis (Q1) et 2les tickets que j'ai crées(Q2)
     return tickets
+
+
+
